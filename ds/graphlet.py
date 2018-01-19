f2a209661b1f2630eb3db4dd988a5ee81ce38a97
Tao-Yang Fu <txf225@CSE-dynpool04.cse.psu.edu>
2018-01-19 10:29:58 -0500

    (ds) speed up graphlet data generation

--- ds/graphlet.py
+++ ds/graphlet.py
@@ -13,59 +13,43 @@ class TrainingSetGenerator():
         self.graph = graph
 
     def generate(self, count, length, window, seed=None):
+        def complete_and_count_degrees(window, walk):
+            for i in xrange(len(walk)-1):
+                nodes = walk[i:i+window+1]
+                id2count = {nodes[0]: 1, nodes[1]: 1}
+                yield id2count
+
+                i = 2
+                while i < len(nodes):
+                    to_id = nodes[i]
+                    if to_id in id2count:
+                        break
+
+                    id2count[to_id] = 1
+                    id2count[nodes[i-1]] += 1
+                    for from_id in nodes[:i-1]:
+                        if to_id in self.graph.graph[from_id]:
+                            id2count[from_id] += 1
+                            id2count[to_id] += 1
+                    i += 1
+                    yield id2count
+
         graph_id2classes = {}
         for class_, ids in self.graph.class_nodes.items():
             for id_ in ids:
                 graph_id2classes[id_] = class_
 
-        c = 0
         matcher = GraphletMatcher()
         for walk in self.graph.random_walks(count, length, seed=seed):
-            for nodes in TrainingSetGenerator.get_nodes(walk, window):
-                for id2degrees in GraphletMatcher.complete_and_count_degrees(self.graph, nodes):
+            for id2degrees in complete_and_count_degrees(window,walk):
+#                   matcher.get_graphlet(graph_id2classes, id2degrees)
 
-                    gid, role_ids, node_ids, node_classes = matcher.get_graphlet(graph_id2classes, id2degrees)
-                    print gid
-                    c += 1
+#                   gid, role_ids, node_ids, node_classes = matcher.get_graphlet(graph_id2classes, id2degrees)
+#                   print gid
+
+                    yield matcher.get_graphlet(graph_id2classes, id2degrees)
 #                   yield gid, role_ids, node_ids, node_classes
-        print c
 
-    @staticmethod
-    def get_nodes(walk, window):
-        for i in xrange(len(walk)-1):
-            yield walk[i:i+window+1]
-
-
-#lass GraphletCompleter():
-
-#   @staticmethod
-#   #TODO handle multi-graphs that two nodes may have more than one edge.
-#   #TODO node_class
-#   def complete(g, nodes, prev_edges=None):
-#       if prev_edges is None:
-#           ext_edges = set([])
-#           for i, from_id in enumerate(nodes[:-1]):
-#               from_tos = g.graph[from_id]
-#               for to_id in nodes[i:]:
-#                   if to_id not in from_tos:
-#                       continue
-#                   for edge_class_id in from_tos[to_id]:
-#                       ext_edges.add((from_id, to_id, edge_class_id))
-#           return ext_edges
-#       else:
-#           to_id = nodes[-1]
-#           for i, from_id in enumerate(nodes[:-1]):
-#               from_tos = g.graph[from_id]
-#               if to_id not in from_tos:
-#                   continue
-#               for edge_class_id in from_tos[to_id]:
-#                   prev_edges.add((from_id, to_id, edge_class_id))
-#           return prev_edges
-
-#   @staticmethod
-#   #TODO implement
-#   def incremental_complete():
-#       pass
 
 
 #FIXME currently, only for less than or equal to 4 nodes, and indirected
@@ -94,45 +78,26 @@ class GraphletMatcher():
             return False
         return True
 
-    @staticmethod
-    def complete_and_count_degrees(g, nodes):
-        id2count = {nodes[0]: 1, nodes[1]: 1}
-        yield id2count
-
-        i = 2
-        while i < len(nodes):
-            to_id = nodes[i]
-            if to_id in id2count:
-                break
-
-            id2count[to_id] = 1
-            id2count[nodes[i-1]] += 1
-            for from_id in nodes[:i-1]:
-                if to_id in g.graph[from_id]:
-                    id2count[from_id] += 1
-                    id2count[to_id] += 1
-            i += 1
-            yield id2count
 
     def get_graphlet(self, id2classes, id2degrees):
         '''
             return graphlet_id, role_ids, node_ids, node_classes
         '''
-        deg_class_ids = zip(*sorted([(degree, id2classes[id_], id_)
-                                     for id_, degree
-                                     in id2degrees.items()]))
-        degrees = deg_class_ids[0]
+        degrees, classes, ids = zip(*sorted([(id2degrees[id_], id2classes[id_], id_)
+                                             for id_
+                                             in id2degrees]))
 
         if degrees not in self.template:
             return None, None, None, None
 
-        classes = deg_class_ids[1]
         key = (degrees, classes)
-        if key not in self.graphlets:
-            roles = [self.rid_offset+rid
-                     for rid in self.template[degrees]]
-            self.graphlets[key] = (len(self.graphlets), roles)
-            self.rid_offset = roles[-1]+1
-        gid, role_ids = self.graphlets[key]
+        try:
+            gid, role_ids = self.graphlets[key]
+        except KeyError:
+            role_ids = [self.rid_offset+rid
+                        for rid in self.template[degrees]]
+            gid = len(self.graphlets)
+            self.graphlets[key] = (gid, role_ids)
+            self.rid_offset = role_ids[-1]+1
 
-        return gid, role_ids, deg_class_ids[2], classes
+        return gid, role_ids, ids, classes
